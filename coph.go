6f86dc1691c7b56d6df424c09fea8d0217b4a8d0
Russell Davies <russell@zeroflux.net>
2016-06-15 13:23:15 +0100

    Added basic auth

--- coph.go
+++ coph.go
@@ -19,18 +19,30 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 	"unsafe"
 )
 
 var (
-	server   = flag.String("server", "localhost:631", "CUPS server to connect to")
-	username = flag.String("username", "", "CUPS username")
-	password = flag.String("password", "", "CUPS password")
-	port     = flag.Int("port", 8080, "HTTP server port")
-	addr     string
+	username     = flag.String("username", "", "coph username")
+	password     = flag.String("password", "", "coph password")
+	port         = flag.Int("port", 6631, "Port that coph listens on")
+	addr         string
+	cupsServer   = flag.String("cups-server", "localhost", "CUPS server to connect to")
+	cupsUsername = flag.String("cups-username", "", "CUPS username")
+	cupsPassword = flag.String("cups-password", "", "CUPS password")
 )
 
+type logWriter struct {
+}
+
+func (writer logWriter) Write(bytes []byte) (int, error) {
+	return fmt.Print(time.Now().UTC().Format(time.RFC3339) + " " + string(bytes))
+}
+
 func main() {
+	log.SetFlags(0)
+	log.SetOutput(new(logWriter))
 	flag.Parse()
 
 	switch {
@@ -38,18 +50,32 @@ func main() {
 		log.Fatalf("Missing required --username parameter")
 	case len(*password) == 0:
 		log.Fatalf("Missing required --password parameter")
+	case len(*cupsUsername) == 0:
+		log.Fatalf("Missing required --cups-username parameter")
+	case len(*cupsPassword) == 0:
+		log.Fatalf("Missing required --cups-password parameter")
 	}
 	addr = ":" + strconv.Itoa(*port)
 
 	http.HandleFunc("/", httpHandler)
 	log.Printf("Starting server on %s", addr)
-	log.Printf("CUPS server is %s", *server)
+	log.Printf("CUPS server is %s", *cupsServer)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func httpHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	authUsername, authPassword, authOK := r.BasicAuth()
+	log.Printf("%b", authOK)
+	switch {
+	case !authOK:
+		http.Error(w, "Basic auth must be supplied", http.StatusUnauthorized)
+		return
+	case authUsername != *username || authPassword != *password:
+		http.Error(w, "Invalid auth", http.StatusUnauthorized)
 		return
 	}
 	statusCode := http.StatusOK
@@ -93,7 +119,7 @@ func httpHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func print(printerName string, buffer *bytes.Buffer, size int64) int {
-	return int(C.streamPrint(C.CString(*server), C.CString(*username),
-		C.CString(*password), C.CString(printerName), nil,
+	return int(C.streamPrint(C.CString(*cupsServer), C.CString(*cupsUsername),
+		C.CString(*cupsPassword), C.CString(printerName), nil,
 		(*C.char)(unsafe.Pointer(&buffer.Bytes()[0])), C.size_t(size)))
 }
